26a8e9026f947bed4d4fda7a4ab147ea1a92903e
Aldo Danilo Vargas Esquivel <el1n53ct054ur103000@gmail.com>
2024-02-25 16:00:44 -0600

    Creacion de la pagina BlueEarth

---
+++ src/app/pages/inicio/inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export default class InicioComponent {
+
+}
